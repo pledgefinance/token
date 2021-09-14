356b94dc6c4af2746bcfb811ac23eba936714ace
Janet Liang <jan.liang96@gmail.com>
2021-09-13 18:37:45 -0700

    [#11] Revert reorg for audit
    Revert "reorganized contracts"
    
    This reverts commit 778086862702e449f0031e7909c6705a90bbfea3.
    
    Revert "added .gitignore"
    
    This reverts commit a4b0e20234a891e0710e2cb48c7d0e279e94dff8.
    
    Revert "added hardhat lib"
    
    This reverts commit c646227266f5e2133dd3023eb55ff6b16eb5da14.

--- PledgeToken.sol
+++ PledgeToken.sol
@@ -1,12 +1,360 @@
-pragma solidity ^0.4.24;
+pragma solidity 0.4.24;
 
-import "./interface/IBEP20.sol";
-import "./interface/IERC1132.sol";
-import "./Ownable.sol";
-import "./Pausable.sol";
+contract BEP20Interface {
 
-contract Token is BEP20Interface, Ownable, Pausable, ERC1132 {
-    
+    function symbol()
+        public
+        view
+        returns (string memory);
+
+    function name()
+        public
+        view
+        returns (string memory);
+
+    function decimals()
+        public
+        view
+        returns (uint8);
+
+    function totalSupply()
+        public
+        view
+        returns (uint256);
+
+    function balanceOf(
+        address _address)
+        public
+        view
+        returns (uint256 balance);
+
+    function allowance(
+        address _address,
+        address _to)
+        public
+        view
+        returns (uint256 remaining);
+
+    function transfer(
+        address _to,
+        uint256 _value)
+        public
+        returns (bool success);
+
+    function approve(
+        address _to,
+        uint256 _value)
+        public
+        returns (bool success);
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value)
+        public
+        returns (bool success);
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+}
+
+
+contract Owned {
+    address owner;
+    address newOwner;
+    uint32 transferCount;
+
+    event TransferInitiated(
+        address indexed _from,
+        address indexed _to
+    );
+
+    event TransferOwnership(
+        address indexed _from,
+        address indexed _to
+    );
+
+    constructor() public {
+        owner = msg.sender;
+        transferCount = 0;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(
+        address _newOwner)
+        public
+        onlyOwner
+    {
+        newOwner = _newOwner;
+        emit TransferInitiated(
+            msg.sender,
+            newOwner
+        );
+    }
+
+    function getOwner()
+        public
+        view
+        returns (address)
+    {
+        return owner;
+    }
+
+    function viewTransferCount()
+        public
+        view
+        onlyOwner
+        returns (uint32)
+    {
+        return transferCount;
+    }
+
+    function isTransferPending()
+        public
+        view
+        returns (bool) {
+        require(
+            msg.sender == owner ||
+            msg.sender == newOwner);
+        return newOwner != address(0);
+    }
+
+    function acceptOwnership()
+         public
+    {
+        require(msg.sender == newOwner);
+
+        owner = newOwner;
+        newOwner = address(0);
+        transferCount++;
+
+        emit TransferOwnership(
+            owner,
+            newOwner
+        );
+    }
+}
+
+library SafeMath {
+    function add(
+        uint256 a,
+        uint256 b)
+        internal
+        pure
+        returns(uint256 c)
+    {
+        c = a + b;
+        require(c >= a);
+    }
+
+    function sub(
+        uint256 a,
+        uint256 b)
+        internal
+        pure
+        returns(uint256 c)
+    {
+        require(b <= a);
+        c = a - b;
+    }
+
+    function mul(
+        uint256 a,
+        uint256 b)
+        internal
+        pure
+        returns(uint256 c) {
+        c = a * b;
+        require(a == 0 || c / a == b);
+    }
+
+    function div(
+        uint256 a,
+        uint256 b)
+        internal
+        pure
+        returns(uint256 c) {
+        require(b > 0);
+        c = a / b;
+    }
+}
+
+contract ApproveAndCallFallBack {
+    function receiveApproval(
+        address _from,
+        uint256 _value,
+        address token,
+        bytes data)
+        public
+        returns (bool success);
+}
+
+
+contract Pausable is Owned {
+  event Pause();
+  event Unpause();
+
+  bool public paused = false;
+
+  modifier whenNotPaused() {
+    require(!paused);
+    _;
+  }
+
+  modifier whenPaused() {
+    require(paused);
+    _;
+  }
+
+  function pause() onlyOwner whenNotPaused public {
+    paused = true;
+    emit Pause();
+  }
+
+  function unpause() onlyOwner whenPaused public {
+    paused = false;
+    emit Unpause();
+  }
+}
+
+
+/**
+ * @title ERC1132 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/1132
+ */
+contract ERC1132 {
+    /**
+     * @dev Reasons why a user's tokens have been locked
+     */
+    mapping(address => bytes32[]) public lockReason;
+
+    /**
+     * @dev locked token structure
+     */
+    struct lockToken {
+        uint256 amount;
+        uint256 validity;
+        bool claimed;
+    }
+
+    /**
+     * @dev Holds number & validity of tokens locked for a given reason for
+     *      a specified address
+     */
+    mapping(address => mapping(bytes32 => lockToken)) public locked;
+
+    /**
+     * @dev Records data of all the tokens Locked
+     */
+    event Locked(
+        address indexed _of,
+        bytes32 indexed _reason,
+        uint256 _amount,
+        uint256 _validity
+    );
+
+    /**
+     * @dev Records data of all the tokens unlocked
+     */
+    event Unlocked(
+        address indexed _of,
+        bytes32 indexed _reason,
+        uint256 _amount
+    );
+
+    /**
+     * @dev Locks a specified amount of tokens against an address,
+     *      for a specified reason and time
+     * @param _reason The reason to lock tokens
+     * @param _amount Number of tokens to be locked
+     * @param _time Lock time in seconds
+     */
+    function lock(bytes32 _reason, uint256 _amount, uint256 _time)
+        public returns (bool);
+
+    /**
+     * @dev Returns tokens locked for a specified address for a
+     *      specified reason
+     *
+     * @param _of The address whose tokens are locked
+     * @param _reason The reason to query the lock tokens for
+     */
+    function tokensLocked(address _of, bytes32 _reason)
+        public view returns (uint256 amount);
+
+    /**
+     * @dev Returns tokens locked for a specified address for a
+     *      specified reason at a specific time
+     *
+     * @param _of The address whose tokens are locked
+     * @param _reason The reason to query the lock tokens for
+     * @param _time The timestamp to query the lock tokens for
+     */
+    function tokensLockedAtTime(address _of, bytes32 _reason, uint256 _time)
+        public view returns (uint256 amount);
+
+    /**
+     * @dev Returns total tokens held by an address (locked + transferable)
+     * @param _of The address to query the total balance of
+     */
+    function totalBalanceOf(address _of)
+        public view returns (uint256 amount);
+
+    /**
+     * @dev Extends lock for a specified reason and time
+     * @param _reason The reason to lock tokens
+     * @param _time Lock extension time in seconds
+     */
+    function extendLock(bytes32 _reason, uint256 _time)
+        public returns (bool);
+
+    /**
+     * @dev Increase number of tokens locked for a specified reason
+     * @param _reason The reason to lock tokens
+     * @param _amount Number of tokens to be increased
+     */
+    function increaseLockAmount(bytes32 _reason, uint256 _amount)
+        public returns (bool);
+
+    /**
+     * @dev Returns unlockable tokens for a specified address for a specified reason
+     * @param _of The address to query the the unlockable token count of
+     * @param _reason The reason to query the unlockable tokens for
+     */
+    function tokensUnlockable(address _of, bytes32 _reason)
+        public view returns (uint256 amount);
+
+    /**
+     * @dev Unlocks the unlockable tokens of a specified address
+     * @param _of Address of user, claiming back unlockable tokens
+     */
+    function unlock(address _of)
+        public returns (uint256 unlockableTokens);
+
+    /**
+     * @dev Gets the unlockable tokens of a specified address
+     * @param _of The address to query the the unlockable token count of
+     */
+    function getUnlockableTokens(address _of)
+        public view returns (uint256 unlockableTokens);
+
+}
+
+
+contract Token is BEP20Interface, Owned, Pausable, ERC1132 {
     using SafeMath for uint256;
 
     string public symbol;
